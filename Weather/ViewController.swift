84fdcb5d8f0eb7d384c4a24845532185f6c0abef
Kapil Amagain <kapil.amagain@gmail.com>
2019-07-03 12:41:35 +1000

    RxSwift, RxCocoa added
    Updated project to Swift 5.0

---
+++ Weather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by User on 3/7/19.
+//  Copyright © 2019 amagain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+        
+    }
+
+
+}
+
