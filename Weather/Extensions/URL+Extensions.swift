3e73454669fa7ab5c700361b3aacb7a0fced2480
Kapil Amagain <kapil.amagain@gmail.com>
2019-07-03 13:18:17 +1000

    Empty Result Error Caught
    Displaying empty weather result in case web request doesn't go through

---
+++ Weather/Extensions/URL+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  URL+Extensions.swift
+//  Weather
+//
+//  Created by User on 3/7/19.
+//  Copyright © 2019 amagain. All rights reserved.
+//
+
+import Foundation
+extension URL {
+    static func urlForWeatherAPI(city: String) -> URL? {
+        return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=73172ed735a442972f75c890958e9eea")
+    }
+}
